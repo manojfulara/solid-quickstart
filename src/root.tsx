9e55b3d45d411083a02229f06d8be63e854f20c6
MANOJ FULARA <64984144+manojfulara@users.noreply.github.com>
2024-02-09 20:30:09 +0530

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -16,17 +16,20 @@ import "./root.css";
 
 export default function Root() {
   return (
-    <Html lang="en">
+    <Html lang="en" class="show-my-ip">
       <Head>
-        <Title>SolidStart - Bare</Title>
+      
+<Title>Show My IP - Your IPv4 and IPv6 Address</Title>
+<Meta name="description" content="Check and display your IPv4 and IPv6 addresses on this website." />
+
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
+{/*             <A href="/">Index</A>
+            <A href="/about">About</A> */}
             <Routes>
               <FileRoutes />
             </Routes>
